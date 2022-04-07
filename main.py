ce3d04ff92ad8d3393c9b3d501949b6a719f161a
Nickkreker <nick_berezikov@me.com>
2022-04-07 19:03:28 +0300

    Add verbose parameter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import click
+import time
 from utils import predict, save_evolution, read_init, plot_evolution
 
 @click.command()
@@ -6,12 +7,28 @@ from utils import predict, save_evolution, read_init, plot_evolution
 @click.argument('output')
 @click.option('--visualize', is_flag=True,
              help='Add visualisation of evolution to output')
-def cli(input, output, visualize):
+@click.option('--verbose', '-v', is_flag=True,
+             help='Print additional runtime information')
+def cli(input, output, visualize, verbose):
+    start_time = time.perf_counter()
+    if verbose:
+        print('Reading input')
+    
     Ed = read_init(input)
+
+    if verbose:
+        print('Calculating evolution')
     evolution = predict(Ed)
+
+    if verbose:
+        print('Saving evolution to file')
     save_evolution(evolution, output)
 
     if visualize:
         plot_evolution(evolution, output)
+
+    elapsed_time = time.perf_counter() - start_time
+    if verbose:
+        print(f'Runtime={elapsed_time}s')
 
 
