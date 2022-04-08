987f94533b61b193e6b9c02f7a666ecb0744a725
Nickkreker <nick_berezikov@me.com>
2022-04-08 23:30:16 +0300

    Fixed formitting issues

--- utils.py
+++ utils.py
@@ -25,16 +25,18 @@ def predict(x, evolution_length=9):
     return Ed.reshape((-1, 256, 256)), Vx.reshape((-1, 256, 256)), Vy.reshape((-1, 256, 256))
 
 def save_component(component, f):
-    short_prefix = 6 * ' '
+    short_prefix = 5 * ' '
     long_prefix = 8 * ' '
 
-    Comp0_str = '\n'.join(list(map(lambda x: short_prefix + np.array2string(x, separator=short_prefix,
-                                                                              floatmode='fixed')[1:-1], component[0])))
+    Comp0_str = '\n'.join(list(map(lambda x: short_prefix + np.array2string(x, separator=short_prefix, max_line_width=10000,
+                                                                            formatter={'float_kind':lambda x:(x>0)*' ' + f'{x:.8f}'})[1:-1], component[0])),)
+    
     Comp0_str = long_prefix + f'{0:.8f}' + '\n' + Comp0_str + '\n'
     f.write(Comp0_str)
+
     for i in range(10):
-        Comp_str = '\n'.join(list(map(lambda x: short_prefix + np.array2string(x, separator=short_prefix,
-                                                                                floatmode='fixed')[1:-1], component[i])))
+        Comp_str = '\n'.join(list(map(lambda x: short_prefix + np.array2string(x, separator=short_prefix, max_line_width=10000,
+                                                                               formatter={'float_kind':lambda x:(x>0)*' ' + f'{x:.8f}'})[1:-1], component[i])))
         Comp_str = long_prefix + f'{i:.8f}' + '\n' + Comp_str + '\n'
         f.write(Comp_str)
 
